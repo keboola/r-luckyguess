6e98dfdca7d5ea4f8c0bff313c6f95d48a3f2ef8
Odin <odinuv@gmail.com>
2015-07-10 10:10:50 +0200

    fix: support for setting token in environment

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,18 @@
 library(testthat)
 
-source("config.R")
+# default values
+KBC_TOKEN = 'yourToken'
+KBC_RUNID = '123'
+DATA_DIR = 'tests/data'
+
+# override with config if any
+if (file.exists("config.R")) {
+    source("config.R")
+}
+
+# override with environment if any
+if (nchar(Sys.getenv("KBC_TOKEN")) > 0) {
+    apitoken <- Sys.getenv("KBC_TOKEN")  
+}
+
 test_check("keboola.r.luckyguess")
