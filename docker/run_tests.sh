e89f230dcda6763f3b45fd9ddc9744840b86e47b
Odin <odinuv@gmail.com>
2016-10-05 16:07:59 +0200

    refactor: it was stupid from the beginning

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-echo "Starting tests" >&1
+echo "Starting tests." >&1
 
 export KBC_DATADIR=/data/
 cd /code/
 R CMD build .
-R CMD check keboola.r.luckyguess_* && grep -q -R "/code/keboola.r.luckyguess.Rcheck/00check.log"
 
-if [ $? -eq 0 ]
-then
-  echo "Test passed successfully."
-  exit 0
+echo "Build finished." >&1
+if [ R CMD check keboola.r.luckyguess_* -eq 0 ] ; then
+	echo "Test passed successfully." >&1
+	if [ grep -q -R "/code/keboola.r.luckyguess.Rcheck/00check.log" ] ; then
+		echo "No warnings found." >&1
+	else
+		echo "Warnings found." >&2
+		cat /code/keboola.r.luckyguess.Rcheck/00check.log
+		exit 1
+	fi
 else
-  echo "Tests failed." >&2
-  cat /code/keboola.r.luckyguess.Rcheck/00check.log
-  exit 1
+	echo "Tests failed." >&2
+	cat /code/keboola.r.luckyguess.Rcheck/00check.log
+	exit 1
 fi
