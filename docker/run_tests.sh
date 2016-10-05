82a420f6008f845bd6a4e34dc0cdf1daec85f7a4
Odin <odinuv@gmail.com>
2016-10-05 14:57:01 +0200

    fix: kbc_datadir env

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Starting tests" >&1
 
+export KBC_DATADIR=/data/
 cd /code/
 R CMD build .
-R CMD check keboola.r.luckyguess_*
-grep -q -R "WARNING" "${RCHECK_DIR}/00check.log"
+R CMD check keboola.r.luckyguess_* && grep -q -R "/code/keboola.r.luckyguess.Rcheck/00check.log"
