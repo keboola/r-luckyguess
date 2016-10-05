d5af42598e6792a1aa54a2192349ee96a15bc494
Odin <odinuv@gmail.com>
2016-10-05 15:19:02 +0200

    fix: display error log

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -5,3 +5,11 @@ export KBC_DATADIR=/data/
 cd /code/
 R CMD build .
 R CMD check keboola.r.luckyguess_* && grep -q -R "/code/keboola.r.luckyguess.Rcheck/00check.log"
+
+if [ $? -eq 0 ]
+then
+  echo "Test passed successfully."
+else
+  echo "Tests failed." >&2
+  cat /code/keboola.r.luckyguess.Rcheck/00check.log
+fi
