a9418dd234f334f9ff8b0db8ddf7f5c3b148bc51
Odin <odinuv@gmail.com>
2016-10-06 12:22:41 +0200

    fix: bug in provisioning

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker login -e="." -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
+docker login -u="$QUAY_USERNAME" -p="$QUAY_PASSWORD" quay.io
 docker tag keboola/r-luckyguess quay.io/keboola/r-luckyguess:$TRAVIS_TAG
 docker tag keboola/r-luckyguess quay.io/keboola/r-luckyguess:latest
 docker images
