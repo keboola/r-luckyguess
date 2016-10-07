0063e16376010eee13b8932d8c096929eb6e76db
Odin <odinuv@gmail.com>
2016-10-07 09:10:16 +0200

    fix: add testthat package and fix backend case naming

--- docker/init.R
+++ docker/init.R
@@ -1,5 +1,5 @@
 # install really required packages
-install.packages(c('git2r', 'jsonlite', 'devtools', 'rJava', 'RJDBC', 'dplyr'), repos = 'http://cran.us.r-project.org', dependencies = c("Depends", "Imports", "LinkingTo"), INSTALL_opts = c("--no-html"))
+install.packages(c('git2r', 'jsonlite', 'devtools', 'rJava', 'RJDBC', 'dplyr', 'testthat'), repos = 'http://cran.us.r-project.org', dependencies = c("Depends", "Imports", "LinkingTo"), INSTALL_opts = c("--no-html"))
 
 # install some commonly used packages
 install.packages(c('corrgram', 'data.table', 'gbm', 'ggplot2', 'leaps', 'plyr', 'lubridate', 'arules', 'reshape2'), repos = 'http://cran.us.r-project.org', dependencies = c("Depends", "Imports", "LinkingTo"), INSTALL_opts = c("--no-html"))
@@ -10,7 +10,7 @@ library('devtools')
 install_github('keboola/redshift-r-client', ref = "1.0.10")
 install_github('snowflakedb/dplyr-snowflakedb', ref="v0.2.0")
 install_github('keboola/provisioning-r-client', ref = "1.0.3")
-install_github('keboola/backend-r-client', ref = "0.0.3")
+install_github('keboola/backend-r-client', ref = "0.0.4")
 install_github('keboola/r-application', ref = "1.0.1")
 install_github('keboola/r-docker-application', ref = "1.0.0")
 
